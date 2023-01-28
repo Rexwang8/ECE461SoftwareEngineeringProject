35bdacdc69d8d69ed5a803a74a5ce453769f2e80
Rexwang8 <rexkingsbackyard@gmail.com>
2023-01-28 18:37:16 -0500

    python logger class

--- Build/startup.py
+++ Build/startup.py
@@ -1,13 +1,17 @@
 import sys
 import time
 import pathlib
+import logger #logger.py
+
 
 #get args
 def main():
     #current working dir
     dir = pathlib.Path().absolute()
+    
+    
    
-    log("Starting main script...", 1, "log.txt")
+    #log("Starting main script...", 1, "log.txt")
     #get first arg as command
     command = sys.argv[1]
     #get second arg LOG LEVEL
@@ -18,14 +22,20 @@ def main():
     #append "log.txt" to current working dir variable
     logpath = str(dir) + "\\" + logfile
     
-    logstr = f"ARgS: {sys.argv}, logpath: {logpath}, cmd: {command}, loglevel: {loglevel}"
-    log(logstr, 2, logpath)
+    Debug = logger.Logger(logpath, 2)
     
-    log("Starting main script...", 1, logpath)
+    Debug.log("Starting main script...")
+    
+    logstr = f"ARgS: {sys.argv}, logpath: {logpath}, cmd: {command}, loglevel: {loglevel}"
+    Debug.log(logstr, 2)
+    
+    Debug.log("Starting main script...", 1)
 
-    log("Testing logging...", 1, logpath)
+    Debug.log("Testing logging...", 1)
     return 0
 
+
+'''
 def log(msg, priority=0, path="log.txt"):
     if(priority == 0):
         return
@@ -39,5 +49,5 @@ def log(msg, priority=0, path="log.txt"):
     with open(path, "a") as f:
         f.write(f"[Python Main Script] Priority: {priority} | Logging at t={time.time()}: {msg}\n")
         f.close()
-        
+        '''
 main()
