d7bf3e3382ebcdc7c4c0173ea41371275ebdce93
Rex Wang <85261458+Rexwang8@users.noreply.github.com>
2023-01-28 15:57:44 -0500

    merge rex branch with main hopefully (#6)
    * Update README.md
    
    * Commit by Alan Zhang
    
    * Last Commit by Kevin
    
    * Commit by Joseph Ma
    
    * Update README.md
    
    * fixed the readme format - Kevin
    
    * Joseph branch
    
    * Joseph's commit part 2
    
    * Create .gitignore
    
    * Update README.md
    
    * CLI parser skeleton
    
    * test
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Logging and stuff
    
    ---------
    
    Co-authored-by: Joseph Ma <55027668+ma562@users.noreply.github.com>
    Co-authored-by: Azhang1234 <zhan4096@purdue.edu>
    Co-authored-by: Lin <lin1285@purdue.edu>
    Co-authored-by: Joseph Ma <Josephma@pal-10-184-34-189.itap.purdue.edu>
    Co-authored-by: Kevin Lin <70678769+5OL4M3@users.noreply.github.com>

---
+++ CLIParser/Startup.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+// Hello World! program
+namespace PackageManager
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //get environment variables
+            int ENVLOGLEVEL = 2;
+            string ENVLOGLOCATION = "log.txt";
+
+            //KEEP THIS COMMENTED -------------- FOR DEBUGGING
+            /*
+            bool result = Int32.TryParse(Environment.GetEnvironmentVariable("$LOG_LEVEL"), out ENVLOGLEVEL);
+
+            if (!result)
+            {
+                Console.WriteLine("Error: Environment variable $LOG_LEVEL is not set or set properly");
+                Environment.Exit(1);
+            }
+
+            
+            //ENVLOGLOCATION = Environment.GetEnvironmentVariable("$LOG_FILE");
+            */
+
+
+            //get command line arguments
+            if (args.Length > 0)
+            {
+                if (ENVLOGLEVEL > 1)
+                {
+                    Console.WriteLine("Command line arguments:");
+                    foreach (string arg in args)
+                    {
+                        Console.WriteLine(arg);
+                    }
+                }
+            }
+
+            
+            //check if arg 0 is "install", "build", "test"
+            if (args[0] == "install")
+            {
+                Console.WriteLine("Installing...");
+                //Installs any dependencies we need, including python packages
+            }
+            else if (args[0] == "build")
+            {
+                Console.WriteLine("Building...");
+                //builds anything we need, I'm not sure we need anything here...
+            }
+
+            else if (args[0] == "test")
+            {
+                Console.WriteLine("Testing...");
+                //This should launch a static analysis script
+            }
+            else
+            {
+                //Test if the arg is a url
+                if (Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
+                {
+                    Console.WriteLine($"Downloading from URL {args[0]}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command, exiting...");
+                    Environment.Exit(1);
+                }
+            
+            }
+
+            
+
+            //hang the program for 1 seconds
+            Console.WriteLine("Hang the program for 1 seconds");
+            System.Threading.Thread.Sleep(1000);
+
+            //instantiate class Startup
+            Startup startup = new Startup();
+            //startup.RunCommand("/C mkdir testdir");
+            startup.LogToFile($"/C python startup.py {args[0]} {ENVLOGLEVEL} {ENVLOGLOCATION}", ENVLOGLEVEL);
+            startup.RunCommand($"/C python startup.py {args[0]} {ENVLOGLEVEL} {ENVLOGLOCATION}");
+            startup.LogToFile("Hello World!", ENVLOGLEVEL);
+
+
+        }
+
+        //define function for running a command
+        void RunCommand(string command)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        //defines a function that is able to log out to log file
+        /*Your software must produce a log file stored in the location named in the environment variable
+         * $LOG_FILE and using the verbosity level indicated in the environment variable $LOG_LEVEL 
+         * (0 means silent, 1 means informational messages, 2 means debug messages).
+         * Default log verbosity is 0.*/
+        void LogToFile(string text, int priority)
+        {
+            string logFile = "log.txt";
+            string logLine = "[C# Command Parser] " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " Priority " + priority.ToString() + " | " + text;
+            File.AppendAllText(logFile, logLine + Environment.NewLine);
+        }
+
+    }
+
+
+
+}
--- README.md
+++ README.md
@@ -1,9 +1,40 @@
 # ECE461SoftwareEngineeringProject
  ECE 461 - Purdue Package manager for Software Engineering class.
 
+Names: Rex Wang, Joseph Ma, Alan Zhang, Kevin Lin
 
-Names: Rex Wang, 
+A local CLI package manager project for ECE 461 - Software engineering (At purdue). 
+
 
 ---
-dummy commits:
-Commit by Rex Wang
+
+## General execution pattern
+
+(C#) CLI command parser is run first. We do this because c# is compiled and we can compile an executable with it to be run indepdantly. It will be able to verify python and other dependancies is installed and run the python main script.
+
+The main script will call all the api calls and other data parsing in order for the program to function. Execution log is saved to /log.txt
+
+## File structure
+
+README.md
+.gitignore
+.git
+.gitattributes
+.vs
+>Command line Parser
+>Command line Parser/Command line parser SLN and other files
+>Build
+>Build/test.py
+>Build/run.exe
+
+---
+
+## How to build CLI Parser (C#)
+
+1. Open .sln in visual studio
+2. go to the top bar, Build > Build Solution
+3. go to the folder /bin/debug/run.exe and copy it out to wherever you want to run it. Make sure it's in the same folder as the python files it's trying to run.
+
+---
+
+
