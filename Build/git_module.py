6c607dd55467cb851a2317a5ba4fa641dcfa1d04
Joseph Ma <Josephma@pal-10-184-32-134.itap.purdue.edu>
2023-02-05 15:53:37 -0500

    github api token environment variable

---
+++ Build/git_module.py
@@ -0,0 +1,52 @@
+import json
+import requests
+import sys
+import os
+
+if len(sys.argv) != 2:
+    print("Error: URL argument missing")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+# Example GraphQL query to retrieve repository information
+query = """
+query {
+  repository(owner:"octocat", name:"Hello-World") {
+    name
+    description
+    createdAt
+    updatedAt
+    diskUsage
+    homepageUrl
+    pushedAt
+    forkCount
+    stargazers {
+      totalCount
+    }
+    }
+}
+"""
+
+headers = {
+    "Authorization": "Bearer " + os.environ.get("GITHUB_API_TOKEN"),
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json={'query': query}, headers=headers)
+
+# Check for API response status
+if response.status_code != 200:
+    print("Failed to retrieve repository information.")
+    sys.exit(1)
+
+# Write JSON response to output file
+with open("output.json", "w") as f:
+    json.dump(response.json(), f)
+print(response)
+print("Repository information successfully written to output.json.")
+
+
+
+
+
