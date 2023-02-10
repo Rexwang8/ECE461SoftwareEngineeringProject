a7531c6bae1e08817e2ed35408a39300a8518724
Joseph Ma <Josephma@pal-10-184-3-82.itap.purdue.edu>
2023-02-10 16:40:28 -0500

    command line args 1st arg (name of json file) 2nd arg (github url)

--- Build/git_module.py
+++ Build/git_module.py
@@ -7,82 +7,65 @@ if len(sys.argv) != 3:
     print("Error: URL argument missing")
     sys.exit(1)
 
-url = sys.argv[2]
+def parse_github_url(url):
+    return url.split('/')[-2], url.split('/')[-1]
 
-the_owner = "Rexwang8"
-the_repo = "ECE461SoftwareEngineeringProject"
+the_owner, the_repo = parse_github_url(sys.argv[2])
 
 headers = {
     "Authorization": "Bearer " + os.environ.get("GITHUB_API_TOKEN"),
     "Content-Type": "application/json"
 }
-'''
-HEADERS = {
-    "Authorization": f"Token {os.environ.get('GITHUB_API_TOKEN')}",
-    "Accept": "application/vnd.github.vUL-preview+json",
-}
 
-def get_security_vulnerabilities(owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}/vulnerabilities"
-    response = requests.get(url, headers=HEADERS)
-    if response.status_code == 200:
-        vulnerabilities = response.json()
-        for vulnerability in vulnerabilities:
-            print(vulnerability["id"], vulnerability["package_name"], vulnerability["severity"])
-    else:
-        print(f"Error retrieving security vulnerabilities: {response.text}")
-
-get_security_vulnerabilities(OWNER, REPO)
-'''
 # Example GraphQL query to retrieve repository information
 query = """
-query {
-  repository(owner:the_owner, name:the_repo) {
+query {{
+  repository(owner: "{}", name: "{}") {{
     name
     description
     createdAt
     updatedAt
     diskUsage
-    licenseInfo {
+    licenseInfo {{
       name
       spdxId
-    } 
+    }}
     isEmpty
     isDisabled
     isFork
     isPrivate
-    watchers(first:100) {
-      nodes {
+    watchers(first:100) {{
+      nodes {{
         login
-      }
-    }
-    collaborators(first: 100) {
-      nodes {
+      }}
+    }}
+    collaborators(first: 100) {{
+      nodes {{
         login
         name
         email
         avatarUrl
-      }
-    }
-    discussions(first: 100) {
-      edges {
-        node {
+      }}
+    }}
+    discussions(first: 100) {{
+      edges {{
+        node {{
           id
           body
           createdAt
           updatedAt
-        }
-      }
-    }
+        }}
+      }}
+    }}
     homepageUrl
     pushedAt
     forkCount
-    stargazers {
+    stargazers {{
       totalCount
-    }
-    }
-}
-"""
+    }}
+  }}
+}}
+""".format(the_owner, the_repo)
 
 response = requests.post("https://api.github.com/graphql", json={'query': query}, headers=headers)
 
@@ -97,8 +80,3 @@ with open(sys.argv[1] + ".json", "w") as f:
     print(response.json())
 print(response)
 print("Repository information successfully written to " + sys.argv[1] + ".json")
-
-
-
-
-
