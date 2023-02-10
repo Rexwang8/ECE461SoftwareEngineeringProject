6ebb1c48e7483af267428fe7a492d86af63e7af0
Joseph Ma <55027668+ma562@users.noreply.github.com>
2023-02-10 17:01:54 -0500

    Update git_module.py

--- github_api/git_module.py
+++ github_api/git_module.py
@@ -3,86 +3,71 @@ import requests
 import sys
 import os
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 4:
     print("Error: URL argument missing")
     sys.exit(1)
 
-url = sys.argv[1]
+def parse_github_url(url):
+    return url.split('/')[-2], url.split('/')[-1]
 
-the_owner = "Rexwang8"
-the_repo = "ECE461SoftwareEngineeringProject"
+the_owner, the_repo = parse_github_url(sys.argv[2])
 
 headers = {
-    "Authorization": "Bearer " + os.environ.get("GITHUB_API_TOKEN"),
+    "Authorization": "Bearer " + sys.argv[3],	#the token
     "Content-Type": "application/json"
 }
-'''
-HEADERS = {
-    "Authorization": f"Token {os.environ.get('GITHUB_API_TOKEN')}",
-    "Accept": "application/vnd.github.vUL-preview+json",
-}
-def get_security_vulnerabilities(owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}/vulnerabilities"
-    response = requests.get(url, headers=HEADERS)
-    if response.status_code == 200:
-        vulnerabilities = response.json()
-        for vulnerability in vulnerabilities:
-            print(vulnerability["id"], vulnerability["package_name"], vulnerability["severity"])
-    else:
-        print(f"Error retrieving security vulnerabilities: {response.text}")
-get_security_vulnerabilities(OWNER, REPO)
-'''
+
 # Example GraphQL query to retrieve repository information
 query = """
-query {
-  repository(owner:"Rexwang8", name:"ECE461SoftwareEngineeringProject") {
+query {{
+  repository(owner: "{}", name: "{}") {{
     name
     description
     createdAt
     updatedAt
     diskUsage
-    licenseInfo {
+    licenseInfo {{
       name
       spdxId
-    } 
+    }}
     isEmpty
     isDisabled
     isFork
     isPrivate
-    watchers(first:100) {
-      nodes {
+    watchers(first:100) {{
+      nodes {{
         login
-      }
-    }
-    collaborators(first: 100) {
-      nodes {
+      }}
+    }}
+    collaborators(first: 100) {{
+      nodes {{
         login
         name
         email
         avatarUrl
-      }
-    }
-    discussions(first: 100) {
-      edges {
-        node {
+      }}
+    }}
+    discussions(first: 100) {{
+      edges {{
+        node {{
           id
           body
           createdAt
           updatedAt
-        }
-      }
-    }
+        }}
+      }}
+    }}
     homepageUrl
     pushedAt
     forkCount
-    stargazers {
+    stargazers {{
       totalCount
-    }
-    }
-}
-"""
+    }}
+  }}
+}}
+""".format(the_owner, the_repo)
 
-response = requests.post(url, json={'query': query}, headers=headers)
+response = requests.post("https://api.github.com/graphql", json={'query': query}, headers=headers)
 
 # Check for API response status
 if response.status_code != 200:
@@ -90,7 +75,8 @@ if response.status_code != 200:
     sys.exit(1)
 
 # Write JSON response to output file
-with open("output.json", "w") as f:
+with open(sys.argv[1] + ".json", "w") as f:
     json.dump(response.json(), f)
+    print(response.json())
 print(response)
-print("Repository information successfully written to output.json.")
+print("Repository information successfully written to " + sys.argv[1] + ".json")
