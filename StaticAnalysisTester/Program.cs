bbe0cc1b34c00d2deb1873e67bae20cd15d80406
rexwang8 <rexkingsbackyard@gmail.com>
2023-02-11 18:05:10 -0500

    static tester

--- StaticAnalysisTester/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using StaticAnalysisLibrary;
-
-StaticAnalysis StaticAnalysis = new StaticAnalysis();
-StaticAnalysis.Analyze("/home/shay/a/lin1285/ECE461SoftwareEngineeringProject/testRepo", "dog", "/home/shay/a/lin1285/ECE461SoftwareEngineeringProject/test.json");
-
---
+++ StaticAnalysisTesterProj/Program2.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StaticAnalysisLibrary;
+
+StaticAnalysis StaticAnalysis = new StaticAnalysis();
+string currentDirectory = Directory.GetCurrentDirectory();
+string pathToRepoFolder = Path.Combine(currentDirectory, "repo");
+
+//for each folder in the repo folder
+for (int i = 0; i < Directory.GetDirectories(pathToRepoFolder).Length; i++)
+{
+    //get the name of the folder
+    string folderName = Path.GetFileName(Directory.GetDirectories(pathToRepoFolder)[i]);
+    //get the path to the folder
+    string pathToFolder = Path.Combine(pathToRepoFolder, folderName);
+
+    //We save results to results/static/
+    string pathToResultsFolder = Path.Combine(currentDirectory, "results", "static");
+    //get the path to the json file
+    string pathToJson = Path.Combine(pathToResultsFolder, folderName + ".json");
+
+    //analyze the folder
+    StaticAnalysis.Analyze(pathToFolder, folderName, pathToJson);
+}
+
+//StaticAnalysis.Analyze("/home/shay/a/lin1285/ECE461SoftwareEngineeringProject/testRepo", "dog", "/home/shay/a/lin1285/ECE461SoftwareEngineeringProject/test.json");
+//Console.WriteLine("Hello, World!");
