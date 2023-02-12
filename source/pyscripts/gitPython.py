6e6acd66ce128e440691079f5a15d66063f9ba7f
Kevin Lin <lin1285@eceprog2.ecn.purdue.edu>
2023-02-12 14:41:30 -0500

    fixed adding text repo

--- source/pyscripts/gitPython.py
+++ source/pyscripts/gitPython.py
@@ -1,13 +1,15 @@
-from git import Repo
+from git import Repo    
+import os
+
 class pythonGit:
     def pyClone(url, path):
         try:
             Repo.clone_from(url, path)
-            #print("Clone success!")
         except:
-            #print(f"Clone failed for {url}! error: {sys.exc_info()[0]}")
             pass
         
-
+#download test repo
 if __name__ == "__main__":
-    pythonGit.pyClone("https://github.com/nullivex/nodist", "/home/shay/a/lin1285/ECE461SoftwareEngineeringProject/testRepo")
+    pythonGit.pyClone("https://github.com/nullivex/nodist", os.getcwd() + "/../../UnitTest/testrepo")
+
+
