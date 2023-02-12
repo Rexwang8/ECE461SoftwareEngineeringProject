750a086d74c92d5fc2b931a35bf6b6e0ec813d6f
rexwang8 <rexkingsbackyard@gmail.com>
2023-02-12 17:57:24 -0500

    lcov

--- source/pyscripts/line_coverage.py
+++ source/pyscripts/line_coverage.py
@@ -1,16 +1,19 @@
 import coverage
 import os
 import sys
-cov = coverage.Coverage(source=['gitPython','github_api', 'logger'])
+cov = coverage.Coverage(source=['grader', 'gitPython','github_api', 'logger'])
 cov.start()
 
 LOG_PATH = "/Users/Josephma/461_project/source/pyscripts/test.txt"
 LOG_FILE = "/Users/Josephma/461project2/testcache2"
 
-
 #sys.argv[1] is LOG_FILE
 #sys.argv[2] is LOG_LEVEL
 #sys.argv[3] is GITHUB_TOKEN
+
+LOG_FILE = sys.argv[1]
+LOG_PATH = sys.argv[2]
+GHTOKEN = sys.argv[3]
 
 ###########################
 #Testing logger.py
@@ -61,8 +64,8 @@ log_test2.log("msg", 1)
 
 #we're calling from line_coverage.py just to test line coverage
 the_url = "https://github.com/Rexwang8/ECE461SoftwareEngineeringProject" #put in testing parameters
-the_name = "myfile"
-module.run_git_module(the_name, the_url, sys.argv[3])
+the_name = "cache/myfile"
+module.run_git_module(the_name, the_url, GHTOKEN)
 
 ###########################
 #Testing gitPython.py
@@ -73,10 +76,10 @@ gitPython.pythonGit.pyClone("invalidURL","invalidPath")
 ###########################
 #Testing grader.py 
 ##########################
-'''
+
 import grader as grade
-grade.main(sys.argv[2], sys.argv[1])
-'''
+grade.main(LOG_PATH, LOG_FILE)
+
 #save the coverage tests
 cov.stop()
 cov.save()
